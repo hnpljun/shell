7db802402395dc6063fdd068a0312fcfca664fa9
lujun2 <lujun9447@hozonauto.com>
2022-09-29 14:24:25 +0800

    update

--- tcpdump.sh
+++ tcpdump.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 source /etc/profile
+
+speechDir="/opt/jidaPki/tcpdump/speech-pki-uat"
 tcpdumppid=`ps -ef | grep tcpdump  | grep 18444 | awk '{print $2}'`
 
-if [ -n ${tcpdumppid} ]; then
-    kill -9 ${tcpdumppid}
+find ${speechDir} -type f -mtime +1 -exec rm -f {} \;
+
+if [[ -n ${tcpdumppid} ]]; then
+    for i in ${tcpdumppid}; do
+        kill -9 ${tcpdumppid}
+    done
 fi
 
 ctime=`date +'%Y%m%d%H%M%S'`
-tcpdump tcp port 18444 or 30090 -w /data/${ctime}.pcap &
-###00 * * * * /usr/bin/sh /data/tcpdump.sh
+tcpdump tcp port 18444 or 30090 -w ${speechDir}/speech-pki-uat_${ctime}.pcap &
