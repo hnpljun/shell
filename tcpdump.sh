dca9c2e41ea6ea40734ae5f0ac467de63ecfe684
lujun2 <lujun9447@hozonauto.com>
2023-03-09 14:24:47 +0800

    update

--- tcpdump.sh
+++ tcpdump.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
-
+#配置crontab，按天、小时、分钟生成文件
+#天：00 00 * * *
+#小时：00 * * * *
+#分钟：*/10 * * * *
+#00 * * * * /usr/bin/sh /opt/jidaPki/tcpdump/tcpdump.sh "/media" "gre_21000" "port_21000"
 source /etc/profile
 
-baseDir="/opt/jidaPki/tcpdump"
-baseName="speech-pki-uat"
+baseDir="${1}"
+baseName="${2}"
 pcapDir="${baseDir}/${baseName}"
-tcpdumpParam="port 18444 or 30090 or icmp"
+#tcpdumpParam="port 18444 or 30090 or icmp"
+tcpdumpParam="${3}"
 tcpdumpPid=`ps -ef | grep tcpdump  | grep "${baseName}" | awk '{print $2}'`
 
 if [[ -n ${tcpdumpPid} ]]; then
-    for i in ${tcpdumpPid}; do
-	kill -9 ${tcpdumpPid}
-    done
+  for i in ${tcpdumpPid}; do
+    kill -9 ${tcpdumpPid}
+  done
 fi
 
 ctime=`date +'%Y%m%d%H%M%S'`
 tcpdump ${tcpdumpParam} -w ${pcapDir}/${baseName}_${ctime}.pcap &
 
-find ${pcapDir} -type f -mtime +1 -exec rm -f {} \;
+#文件保存时间
+saveDays=7
+find ${pcapDir} -type f -mtime +${saveDays} -exec rm -f {} \;
